fd286d808966a24d06d67f695ffabdfbf4c5c6a2
sylvain hervy <sylvain.hervy@gmail.com>
2018-11-28 11:51:45 +0100

    Add GET endpoint

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,3 +12,16 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_get_valid_product(self):
+        response = self.client.get("/api/v1/products/2")
+        self.assertEqual(response.status_code, 200)
+        product = response.json
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['id'], 2)
+        self.assertEqual(product['name'], 'Socialive.tv')
+
+    def test_get_unknown_product(self):
+        response = self.client.get("/api/v1/products/5")
+        self.assertEqual(response.status_code, 404)
+

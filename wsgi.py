4bc1cc6e272d94a0b9a494249f59258f410e0ee9
sylvain hervy <sylvain.hervy@gmail.com>
2018-11-28 11:00:06 +0100

    Add products endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World?"
+
+@app.route('/api/v1/products')
+def products():
+    PRODUCTS = [
+            { 'id': 1, 'name': 'Skello' },
+            { 'id': 2, 'name': 'Socialive.tv' }
+            ]
+    return jsonify(PRODUCTS)
+

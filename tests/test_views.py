2e45b3868e5702845f220b865f0205d4c755a435
sylvain hervy <sylvain.hervy@gmail.com>
2018-11-28 12:27:55 +0100

    Add delete endpoint

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,3 +12,26 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_get_valid_product(self):
+        response = self.client.get("/api/v1/products/2")
+        self.assertEqual(response.status_code, 200)
+        product = response.json
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['id'], 2)
+        self.assertEqual(product['name'], 'Socialive.tv')
+
+    def test_get_unknown_product(self):
+        response = self.client.get("/api/v1/products/65534")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_unknown_product(self):
+        response = self.client.delete("/api/v1/products/65534")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_valid_product(self):
+        response = self.client.delete("/api/v1/products/3")
+        self.assertEqual(response.status_code, 204)
+        response = self.client.get("/api/v1/products/3")
+        self.assertEqual(response.status_code, 404)
+
